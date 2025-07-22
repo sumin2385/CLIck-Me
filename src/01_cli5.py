ac3e941c5afefc0ba240d04e39908e9a58a770bf
daehyun99 <daehyun99@kakao.com>
2025-07-22 18:21:08 +0900

    Docs: 주석 수정 && Feat: 실습 8, 9, 10 추가

--- src/01_cli5.py
+++ src/01_cli5.py
@@ -1,12 +1,12 @@
 ### 실습5 ###
-# 실행 명령어: python src/01_cli5.py
+# 실행 명령어: python [본인_이름]/01_cli5.py
 
 # ==========|코드 실습|========= #
 import click
 
 @click.command()
 def main():
-    pass
+    ...
 
 if __name__ == "__main__":
     main()
--- src/01_cli6.py
+++ src/01_cli6.py
@@ -1,12 +1,12 @@
 ### 실습6 ###
-# 실행 명령어: python src/01_cli6.py
+# 실행 명령어: python [본인_이름]/01_cli6.py
 
 # ==========|코드 실습|========= #
 import click
 
 @click.command()
 def main():
-    pass
+    ...
 
 if __name__ == "__main__":
     main()
